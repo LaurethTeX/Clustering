c9a2cd0b7e4acb3f072984955ae93c466d7e6d7f
LaurethTeX <Laureth@hyperion.astro.uwo.ca>
2014-08-05 13:55:06 -0400

    Labels and title

--- NEDtoREGION-FILE/NEDtoreg.py
+++ NEDtoREGION-FILE/NEDtoreg.py
@@ -70,7 +70,8 @@ def nedtoreg(file_name):
     DEC_text = -29.798166
     
     #Index for labels
-    label = [0,]
+    label = [0 for i in range(25)]
+    
     
     #Extrating data from NED file
     data = np.genfromtxt(file_name,dtype=[('Object_Name','S20'),('RA_deg','f8'),('DEC_deg','f8'),('Type','S6')],delimiter="|",skip_header=25,missing_values='',filling_values=0.0,usecols={1,2,3,4},invalid_raise=False)
@@ -79,120 +80,196 @@ def nedtoreg(file_name):
     region_file = open(file_name[0:-4]+'_REGION.reg','w')
     
     #Write default properties
-    region_file.write('# Region file format: DS9 version 4.1\nglobal color=cyan dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\nfk5\n')
+    region_file.write('# Region file format: DS9 version 4.1\nglobal color=cyan dashlist=8 3 width=1 font="helvetica 12 bold roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\nfk5\n')
     
     #Asign simbols to each interstellar object and write in Region file
     for i in range(data.size):
         #Circle
         if data['Type'][i] == 'SNR':
-            if label[0] == 0:
+            if label[0]%10 == 0:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = cyan text = {Supernova remnant}\n')
-                label[0] = 1
+                label[0]+=1
             else:    
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = cyan\n') 
 
         elif data['Type'][i] == 'HII':
-            if label[1] == 0:
+            if label[1]%10 == 0:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = green text = {HII region}\n')
-                label[1] = 1
+                label[1]+=1
             else:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = green\n')
             
         elif data['Type'][i] == '*Cl':
-            if label[2] == 0:
+            if label[2]%10 == 0:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = blue text = {Star cluster}\n')
-                label[2] == 1
+                label[2]+=1
             else:   
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = blue\n')
             
         elif data['Type'][i] == 'PN':
-            if label[3] == 0:
+            if label[3]%10 == 0:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = yellow text = {Planetary nebula}\n')
-                label[3] = 1
+                label[3]+=1
             else:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = yellow\n')
             
         elif data['Type'][i] == 'MCld':
-            if label[4] == 0:
+            if label[4]%10 == 0:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = white text = {Molecular cloud}\n')
-                label[4] = 1
+                label[4]+=1
             else:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = white\n')
             
         elif data['Type'][i] == 'RfN':
-            if label[5] == 0:
+            if label[5]%10 == 0:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = red text = {Reflection nebula}\n')
-                lable[5] = 1
+                lable[5]+=1
             else:
                 region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = red\n')
             
         elif data['Type'][i] == 'SN':
-            region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = magenta\n')
+            if label[6]%10 == 0:
+                region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = magenta text = {Supernova}\n')
+                label[6]+=1
+            else:
+                region_file.write('circle('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+',1") # color = magenta\n')
                 
         #Diamond 
         elif data['Type'][i] == 'exG*':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = green\n')
+            if label[7]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = green text = {Extragalactic star}\n')
+                label[7]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = green\n')
 
         elif data['Type'][i] == 'G':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = magenta\n')
+            if label[8]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = magenta text = {Galaxy}\n')
+                label[8]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = magenta\n')
             
         elif data['Type'][i] == 'GClstr':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = red\n')
+            if label[9]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = red text = {Cluster of galaxies}\n')
+                label[9]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=diamond color = red\n')
         
         #Cross point(204.24799,-29.830171) # point=cross
         elif data['Type'][i] == '*':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = green\n')
+            if label[10]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = green text = {Star}\n')
+                label[10]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = green\n')
             
         elif data['Type'][i] == '**':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = red\n')
+            if label[11]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = red text = {Double star}\n')
+                label[11]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = red\n')
             
         elif data['Type'][i] == '*Ass':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = white\n')
+            if label[12]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = white text = {Stellar association}\n')
+                label[12]+=1
+            else:    
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = white\n')
             
         elif data['Type'][i] == 'XrayS':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = cyan\n')
+            if label[13]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = cyan text = {X-ray source}\n')
+                label[13]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = cyan\n')
         
         elif data['Type'][i] == 'Blue*':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = blue\n')
+            if label[14]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = blue text = {Blue Star}\n')
+                label[14]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = blue\n')
         
         elif data['Type'][i] == 'C*':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = yellow\n')
+            if label[15]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = yellow text = {Carbon star}\n')
+                label[15]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = yellow\n')
         
         elif data['Type'][i] == 'UvS':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = magenta\n')
+            if label[16]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = magenta text = {Ultraviolet source}\n')
+                label[16]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=cross color = magenta\n')
             
         #Box
         elif data['Type'][i] == 'VisS':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = green\n')
+            if label[17]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = green text = {Visual source}\n')
+                label[17]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = green\n')
         
         elif data['Type'][i] == 'RadioS':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = red\n')
+            if label[18]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = red text = {Radio source}\n')
+                label[18]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = red\n')
             
         elif data['Type'][i] == 'IrS':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = yellow\n')
+            if label[19]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = yellow color = {Infrared source}\n')
+                label[19]+=1
+            else:    
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = yellow\n')
         
         elif data['Type'][i] == 'GGroup':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = blue\n')
+            if label[20]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = blue text = {Group of galaxies }\n')
+                label[20]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=box color = blue\n')
             
         #X
         elif data['Type'][i] == 'UvES':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = magenta\n')
+            if label[21]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = magenta text = {Ultraviolet excess source}\n')
+                label[21]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = magenta\n')
             
         elif data['Type'][i] == 'V*':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = red\n')
+            if label[22]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = red color = {Variable star}\n')
+                label[22]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = red\n')
             
         elif data['Type'][i] == 'PofG':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = white\n')
+            if label[23]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = white text = {Part of galaxy}\n')
+                label[23]+=1
+            else:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=X color = white\n')
             
         #Boxcirle
         elif data['Type'][i] == 'Neb':
-            region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=boxcircle color = cyan\n')
+            if label[24]%10 == 0:
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=boxcircle color = cyan text = {Nebula}\n')
+                label[24]+=1
+            else:    
+                region_file.write('point('+str(data['RA_deg'][i])+','+str(data['DEC_deg'][i])+') # point=boxcircle color = cyan\n')
             
         else:
             print data['Type'][i]
      
-    region_file.write('# text('+str(RA_text)+','+str(DEC_text)+') text={Identified objects in M83}\n')
-    region_file.write('# text('+str(RA_text)+','+str(DEC_text-0.008935)+') text={Main Information Table for NED objects within 10.000 arcmin of object MESSIER 083}\n')
+    region_file.write('# text('+str(RA_text)+','+str(DEC_text)+') text={Identified objects in M83} color = black\n')
+    region_file.write('# text('+str(RA_text)+','+str(DEC_text-0.008935)+') text={Main Information Table for NED objects within 10.000 arcmin} color = black\n')
          
     region_file.close()		
     return 'Number of objects identified: '+str(data.size-1)
